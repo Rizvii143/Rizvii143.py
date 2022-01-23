55cca67c453f31598ee70cfbf5a4b6e3ee70813e
Rizvii <98278873+Rizvii143@users.noreply.github.com>
2022-01-24 00:42:18 +0600

    Create Rizvii143.py
    apt update
    apt upgrade
    apt install python
    pip install mechanize lolcat
    pip install requests bs4
    pip install futures
    apt install git
    git clone https://github.com/Rizvii143/Rizvii143.py.git
    cd old-id-cloner
    python oldcr2k.py

---
+++ Rizvii143.py
@@ -0,0 +1,134 @@
+<img src="https://github.com/Rizvii143/Rizvii143/blob/main/IMAGE/azimvau.gif" width="120" height="120" align="left">
+
+<center>
+
+  
+
+  
+
+ 
+
+   ##  MY SOCIAL MEDIA : <br>
+
+<a href="https://Instagram.com/azimmahmud143" target="_blank"><img src="https://github.com/Rizvii143/Rizvii143/blob/main/IMAGE/instagram.png" alt="alt text" width="25" height="25"></a> 
+
+<a href="https://t.me/mrerror69"><img src="https://github.com/Rizvii143/Rizvii143/blob/main/IMAGE/telegram.png" alt="alt text" width="25" height="25"></a>
+
+<a href="https://www.facebook.com/Owner.of.ur.heart.bby" target="_blank"><img src="https://github.com/Rizvii143/Rizvii143/blob/main/IMAGE/facebook.png" alt="alt text" width="25" height="25"></a> <a href="https://youtube.com/MrError69"><img src="https://github.com/Rizvii143/Rizvii143/blob/main/IMAGE/youtube.png" alt="alt text" width="25" height="25"></a> 
+
+&nbsp;&nbsp;     &nbsp;&nbsp;    &nbsp;&nbsp;   &nbsp;&nbsp;   &nbsp;&nbsp;
+
+  
+
+____Programming And Memes____
+
+CONTACT WITH <a href="https://github.com/Rizvii143"><b>MR. ERROR </a> </br><br>
+
+<img src="https://github.com/Rizvii143/Rizvii143/blob/main/IMAGE/contact.png" alt="alt text" width="25" height="25"> <br>
+
+<p>rizvismilin@gmail.com</p>  <br> <br> 
+
+<a href="https://github.com/Rizvii143/followers">
+
+<img title="Followers" src="https://img.shields.io/github/followers/Rizvii143?label=Followers&color=blue&style=flat-square"></a>
+
+<br>
+
+  <a href="https://github.com/Rizvii143/termux-style/stargazers/">
+
+  <a href="https://github.com/Rizvii143/oldcr2k">
+
+    <img alt="Last Commit" src="https://img.shields.io/github/last-commit/Rizvii143/oldcr2k.svg"/>
+
+  </a>
+
+<br>
+
+  <a href="https://github.com/Rizvii143/oldcr2k">
+
+    <img alt="Language" src="https://img.shields.io/github/languages/count/Rizvii143/oldcr2k.svg"/>
+
+  </a>
+
+  <a href="https://github.com/Azim-vau/oldcr3k">
+
+    <img alt="Starts" src="https://img.shields.io/github/stars/Rizvii143/oldcr2k.svg"/>
+
+  </a>
+
+<br>
+
+<a href="https://github.com/Rizvii143/oldcr2k">
+
+    <img alt="Repo Size" src="https://img.shields.io/github/repo-size/Rizvii143/oldcr2k.svg"/>
+
+  </a>
+
+<br>
+
+<a href="https://github.com/Rizvii143/oldcr2k">
+
+    <img alt="Top Language" src="https://img.shields.io/github/languages/top/Azim-vau/oldcr2k.svg"/> <a                                                                                                        href="https://github.com/Rizvii143/oldcr2k">
+
+    <img alt="Forks" src="https://img.shields.io/github/forks/Rizvii143/oldcr2k.svg"/>
+
+  </a>
+
+</div>
+
+</br>
+
+<p align="center">
+
+      FACEBOOK ACCOUNT CLONER
+
+</p>
+
+#### INSTALL TOOL ON TERMUX
+
+```python
+
+$ apt update && apt upgrade
+
+$ apt install python
+
+$ pip install mechanize lolcat
+
+$ pip install requests bs4
+
+$ pip install futures
+
+$ apt install git
+
+$ git clone https://github.com/Rizvii143/oldcr2k.git
+
+```
+
+#### RUN SCRIPT
+
+```python
+
+$ cd oldcr2k
+
+$ git pull
+
+$ python oldcr2k.py
+
+```
+
+#### JOIN TELEGRAM GROUP <br>
+
+[![](https://img.shields.io/badge/Telegram-black?logo=Telegram&logoColor=blue&labelColor=black)](https://t.me/Rizvii143)
+
+<br>
+
+#### MY SOCIAL MEDIA
+
+[![](https://img.shields.io/badge/Github-black?logo=Github&logoColor=red&labelColor=black)](https://github.com/Rizvii143) <br>
+
+[![](https://img.shields.io/badge/Facebook-black?logo=Facebook&logoColor=red&labelColor=black)](https://www.facebook.com/Owner.of.ur.heart.bby) <br>
+
+[![](https://img.shields.io/badge/Instagram-black?logo=Instagram&logoColor=red&labelColor=black)](https://www.instagram.com/) <br>
+
+<h2> THANKS FOR VISIT ✘✘ <h2\>
+
